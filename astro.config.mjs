defeb0f26678e9a185548041155afd55403e7d83
AdaBan <ada.banaszak@gmail.com>
2023-04-02 16:45:44 +0200

    Removing SSR config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   vite: {
     css: {
       preprocessorOptions: {
