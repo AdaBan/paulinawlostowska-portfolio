4b08b3181b3292e6786d1fc97321d76f069aa36c
AdaBan <ada.banaszak@gmail.com>
2023-04-02 15:53:07 +0200

    Adding sass

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://adaban.github.io",
+  site: "https://adabanaszak.com",
   base: "/paulinawlostowska-portfolio",
   vite: {
     css: {
